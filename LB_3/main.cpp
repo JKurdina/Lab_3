e1080026abc86dc8c7e80a3669914debc729ce77
Julia Kurdina <kurdina.00@gmail.com>
2022-12-03 16:40:40 +0300

    New

--- LB_3/main.cpp
+++ LB_3/main.cpp
@@ -4,9 +4,9 @@ int main() {
 	string str = "(123+10*2-1)  +20/10\n";
 	cout << str;
 	queue<Lexema> lex_res;
-	//queue<Lexema> lex_res();
  	lex_res = lex(str);
-	cout << lex_res << endl;;
+	cout << lex_res << endl;
+	cout << "------------" << endl;
 
 	return 0;
 }
